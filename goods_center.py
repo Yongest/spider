7173bdc72de35a9bd1ec8efb02d7cbcfbd4818bf
yongest <1046788379@qq.com>
2021-05-12 23:00:49 +0800

    爬虫

--- goods_center.py
+++ goods_center.py
@@ -15,28 +15,43 @@ def get(url):
 response = get('/to-kzz/search')
 
 # while True:
-# print(response.content.decode())
-html = etree.HTML(response.content.decode())
-print(response.content.decode())
+response = response.content.decode()
+html = etree.HTML(response)
+
+# print(html)
 node_list = html.xpath('//div[@class="all_de_clist"]/ul/li')
 
 print(len(node_list))
 for goods in node_list:
 
-    link_detail = goods.xpath('./a[1]/@href')[0]  # 方案详情链接
-    img_src_origin = goods.xpath('./a[1]/img/@data-src')[0]  # 方案原图链接
-    vr_links_origin = goods.xpath('./div/div/div/a/@href')  # 方案vr原来的链接
-
-    img_src = ''
-    h3 = goods.xpath('./a[2]/div/h3/text()')[0].split(' | ')
-    name = h3[0]  # 方案名称
-    style = h3[1]  # 方案style
-    price = goods.xpath('./a[2]/div/p[2]/span/text()')[0].split('￥')[1]  # 方案价格
-    mark = goods.xpath('./a[2]/div/p[1]/span[1]/text()')[0]  # 方案标签
-
-    detail_response = get(link_detail)
-    print(detail_response.content.decode())
+    # name = goods.xpath('./a[1]/@href')[0]  # 产品名称
+    thumb = goods.xpath('./a[1]/img/@data-src')[0]  # 缩略图
+    print(thumb)
     break
+    img_src = goods.xpath('./a[1]/@href')[0]  # 轮播图
+    key = goods.xpath('./a[1]/@href')[0]  # url key 与方案关联
+    code = goods.xpath('./a[1]/img/@data-src')[0]  # 编码，SKU(必填)
+    size = goods.xpath('./a[1]/img/@data-src')[0]  # 规格
+    brand = goods.xpath('./a[1]/img/@data-src')[0]  # 品牌
+    style = goods.xpath('./a[1]/img/@data-src')[0]  # 风格
+    material = goods.xpath('./a[1]/img/@data-src')[0]  # 材质
+    color = goods.xpath('./a[1]/img/@data-src')[0]  # 颜色
+    unit_price = goods.xpath('./a[1]/img/@data-src')[0]  # 单价(必填)
+    unit = goods.xpath('./a[1]/img/@data-src')[0]  # 单位
+    status_sale = goods.xpath('./a[1]/img/@data-src')[0]  # 销售状态(必填)
+    sale_link = goods.xpath('./a[1]/img/@data-src')[0]  # 销售网址
+    supplier = goods.xpath('./a[1]/img/@data-src')[0]  # 供应商
+    area = goods.xpath('./a[1]/img/@data-src')[0]  # 区域
+    cost_price = goods.xpath('./a[2]/div/div/p/span/text()')[0]  # 成本价
+    params = goods.xpath('./a[1]/img/@data-src')[0]  # 参数
+    service = goods.xpath('./a[1]/img/@data-src')[0]  # 服务
+    model = goods.xpath('./a[1]/img/@data-src')[0]  # 型号
+    goods_class = goods.xpath('./a[1]/img/@data-src')[0]  # 分类
+    detail = goods.xpath('./a[1]/img/@data-src')[0]  # 详情
+
+
+    # print(detail_response.content.decode())
+
 
 
 def main():
